eabb0a430aab8ce1e8479602defcd9e3c66225c7
João Pedro De Arruda Rocha <Joo.Rocha@proway.treina>
2022-07-01 14:16:36 -0300

    [feat] Implementing switch

---
+++ controle_de_fluxo/JoãoPedro/switch.dart
@@ -0,0 +1,17 @@
+main() {
+  String formasDeMorrer = "prédio";
+
+  switch (formasDeMorrer) {
+    case "arma":
+      print("meia bomba");
+      break;
+    case "prédio":
+      print("daora");
+      break;
+    case "veneno":
+      print("horrível");
+      break;
+    default:
+      print("batatas");
+  }
+}
