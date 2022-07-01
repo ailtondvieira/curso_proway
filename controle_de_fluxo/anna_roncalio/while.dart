82bfa78e4d42b35fe563573ff08b52eeb8ed4d08
Anna Cecília Roncalio <Anna.Roncalio@proway.treina>
2022-07-01 14:01:54 -0300

    [change] change while.dart.

--- controle_de_fluxo/anna_roncalio/while.dart
+++ controle_de_fluxo/anna_roncalio/while.dart
@@ -1,24 +1,40 @@
 import 'dart:io';
-void main() {
 
+void main() {
+  
+  // exercicio while
   print('Adivinhe o número escolhido de 1 a 20!');
   int number = int.parse(stdin.readLineSync()!);
 
   int luckyNumber = 14;
   int attempt = 3;
 
-  while(number != luckyNumber && attempt > 0) {
+  while (number != luckyNumber && attempt > 0) {
     print('Ainda não... Tente novamente!');
     print('Você ainda tem $attempt tentativas.');
     int.parse(stdin.readLineSync()!);
     attempt--;
 
     if (attempt == 0) {
-      print('Infelizmente, não foi dessa vez, o número escolhido era $luckyNumber');
+      print(
+          'Infelizmente, não foi dessa vez, o número escolhido era $luckyNumber');
     }
   }
 
   if (number == luckyNumber) {
-      print('Você acertou!');
-    }
+    print('Você acertou!');
+  }
+
+  //exemplo while e do-while
+  int contador = 1;
+
+  while (contador <= 50) {
+    print('Contando... $contador');
+    contador++;
+  }
+
+  do {
+    print('Contando... $contador');
+    contador++;
+  } while (contador <= 50);
 }
