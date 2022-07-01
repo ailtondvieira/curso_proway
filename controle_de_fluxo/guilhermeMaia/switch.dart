9c44830fbc0c77b35ab256a6710b9b3f1e307a9f
Guilherme Quirino Maia <Guilherme.Maia@proway.treina>
2022-07-01 14:17:17 -0300

    [feat]exercicio switch

---
+++ controle_de_fluxo/guilhermeMaia/switch.dart
@@ -0,0 +1,17 @@
+void main() {
+  String fruta = 'abacaxi';
+
+  switch (fruta) {
+    case 'morango':
+      print('Cor da fruta $fruta fruta é vermelho');
+      break;
+    case 'banana':
+      print('Cor da fruta $fruta é amarelo');
+      break;
+    case 'abacaxi':
+      print('Cor da fruta $fruta é amarelo');
+      break;
+    default:
+      print('A fruta que você me disse é desconhecida');
+  }
+}
