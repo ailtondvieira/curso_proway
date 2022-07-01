5de3785a5d610ba818798a7ec73d2c686beba26f
Júlia Luciani De Oliveira <Jlia.Oliveira@proway.treina>
2022-07-01 11:47:05 -0300

    [feat]

---
+++ controle_de_fluxo/julia.oliveira/ex1.dart
@@ -0,0 +1,10 @@
+  void main(){
+    bool passouCartao = true;
+    if (passouCartao == true){
+      print('Você pode passar a catraca do ônibus agora');
+    }
+    else
+    {
+      print('Passe o cartão!!!');
+    }
+  }
