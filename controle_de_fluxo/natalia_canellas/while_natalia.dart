b9b2e6fa9d5fc96578d92505892388da5b9d05b3
Natalia Canellas <nataliafcanellas@gmail.com>
2022-07-01 13:48:00 -0300

    [feat] Upload while file

---
+++ controle_de_fluxo/natalia_canellas/while_natalia.dart
@@ -0,0 +1,17 @@
+void main() {
+  //operador de repetição enquanto condição faça isso aqui
+  int contador = 1;
+  while (contador <= 50) {
+    // print('Contanto... $contador');
+    contador++;
+  }
+
+  //usar o while com uma situação escolhida
+
+  int dinheiro = 25;
+  while (dinheiro < 100) {
+    print('Você tem $dinheiro reais na carteira, trabalhe!');
+    dinheiro += 25;
+  }
+  print('Agora você tem dinheiro suficiente, pode dar uns rolês!');
+}
