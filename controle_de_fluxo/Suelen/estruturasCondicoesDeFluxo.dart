f09bcb5bcf89baa304c9b7002c740a201dacb3a6
Suelen Laís Laffin <Suelen.Laffin@proway.treina>
2022-07-01 13:03:02 -0300

    feat

---
+++ controle_de_fluxo/Suelen/estruturasCondicoesDeFluxo.dart
@@ -0,0 +1,50 @@
+// CONDIÇÕES DE FLUXO
+
+
+
+void main(){
+
+//   double nota1 = 10;
+
+//  // IF
+
+//  if (nota1 == 10){
+
+//   print('Está correto!');
+
+//  }
+
+//  // IF E ELSE
+
+//  if (nota1 == 10){
+
+//   print('Está correto!');
+
+//  }else{
+//   print('Está incorreto!');
+//  }
+
+ 
+// bool banhaEstaQuente = true;
+
+// if (banhaEstaQuente == true){
+//   print('Fritar o ovo');
+// }else{
+//   print('Esperar');
+// }
+
+bool sinaleiraAmarela = true;
+bool sinaleiraVermelha = false;
+bool sinaleiraVerde = false;
+
+if (sinaleiraVermelha == true ){
+  print('Pare!');
+}else if(sinaleiraAmarela == true){
+  print('Atenção!');
+}else{
+  print('Pode seguir!');
+};
+
+}
+
+ 
