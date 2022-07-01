c0eb6d2d0000a9b788e7812b918d5847169458f4
Anna Cecília Roncalio <Anna.Roncalio@proway.treina>
2022-07-01 13:53:22 -0300

    [feat] file for.dart and [change] complete while file.

---
+++ controle_de_fluxo/anna_roncalio/for.dart
@@ -0,0 +1,7 @@
+
+void main() {
+
+  for(int contador = 1; contador < 50; contador++) {
+    print('Contando... $contador');
+  }
+}
--- controle_de_fluxo/anna_roncalio/while.dart
+++ controle_de_fluxo/anna_roncalio/while.dart
@@ -1,10 +1,24 @@
-
+import 'dart:io';
 void main() {
 
-  int contador = 1;
+  print('Adivinhe o número escolhido de 1 a 20!');
+  int number = int.parse(stdin.readLineSync()!);
 
-  while(contador <= 50) {
-    print('Contando... $contador');
-    contador++;
+  int luckyNumber = 14;
+  int attempt = 3;
+
+  while(number != luckyNumber && attempt > 0) {
+    print('Ainda não... Tente novamente!');
+    print('Você ainda tem $attempt tentativas.');
+    int.parse(stdin.readLineSync()!);
+    attempt--;
+
+    if (attempt == 0) {
+      print('Infelizmente, não foi dessa vez, o número escolhido era $luckyNumber');
+    }
   }
+
+  if (number == luckyNumber) {
+      print('Você acertou!');
+    }
 }
