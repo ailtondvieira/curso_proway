a300b6598bcb001f4a0341fca7ba93017e45336d
Natalia Canellas <nataliafcanellas@gmail.com>
2022-07-01 11:47:12 -0300

    [feat] Upload file for if challenge

---
+++ controle_de_fluxo/natalia_canellas/if_else_natalia.dart
@@ -0,0 +1,12 @@
+void main() {
+  //Desafio criar uma condição de algo que acontece vindo para o trabalho.
+
+  String semaforo = 'amarelo';
+  if (semaforo == 'verde') {
+    print('Siga!');
+  } else if (semaforo == 'amarelo') {
+    print('Atenção!');
+  } else {
+    print('Pare!');
+  }
+}
