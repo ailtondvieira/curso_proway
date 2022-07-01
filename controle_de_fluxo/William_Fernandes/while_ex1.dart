e30251f81eddc922d881459dc2b0d4f5a352f11d
William De Souza Fernandes <William.Fernandes@proway.treina>
2022-07-01 13:42:03 -0300

    [feat] while ex1

--- controle_de_fluxo/William_Fernandes/while_ex1.dart
+++ controle_de_fluxo/William_Fernandes/while_ex1.dart
@@ -1,12 +1,21 @@
+import 'dart:io';
+
 void main() {
-  
+
 int contador = 1;
-while(contador<=50){
-  print('Contando... $contador');
+int somaTotal = 0;
+
+while(contador<=5){
+  print('Digite um número: ');
+  int num = int.parse(stdin.readLineSync().toString());
+
+  somaTotal +=  num;
+
+
   contador++;
 }
 
-
+print('A soma total é : $somaTotal');
 
 
 
