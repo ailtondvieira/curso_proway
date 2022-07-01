777df1e740d5b85abd6ec85948e555857a1f1111
Lucas Mascarello Perin <perin98@gmail.com>
2022-07-01 14:03:20 -0300

    [change] Adicionando o exercicio de for e do/while

--- controle_de_fluxo/Lucas/for_Lucas.dart
+++ controle_de_fluxo/Lucas/for_Lucas.dart
@@ -1,8 +1,11 @@
+import 'dart:io';
 void main() {
-  int contador = 1;
-
-  do {
-    print("Contando... $contador");
-    ++contador;
-  } while (contador <= 50);
+  print("Informe um número máximo para o contador: ");
+  String? leitor = stdin.readLineSync();
+  int numeroMax = int.parse(leitor!);
+  int contador = 0;
+  for (int i = 0; i < numeroMax; i++) {
+      contador++;
+      print("Contando: $contador");
+  }
 }
