6e67b96aa0061eff2bfe47ad9846c85573896814
Suelen Laís Laffin <Suelen.Laffin@proway.treina>
2022-07-01 15:07:44 -0300

    [feat] add files

---
+++ controle_de_fluxo/Suelen/Switch.dart
@@ -0,0 +1,50 @@
+void main(){
+
+  // String pais = 'Brasil';
+  // String moeda = '';
+
+  // switch (pais) {
+  //   case 'Argentina':
+  //     moeda = 'Peso Argentino';
+  //     break;
+  //   case 'Espanha':
+  //     moeda = 'Euro';
+  //     break;
+
+  //   case 'Brasil':
+  //     moeda = 'Real';
+  //     break;
+
+  //   default:
+  //   moeda = 'Desconhecida';
+
+  // }
+
+  // print('O país é $pais');
+  // print('A moeda é $moeda');
+
+
+  String alimento = 'bolo';
+  String sabor = '';
+
+  switch (alimento) {
+    case 'hamburger':
+        sabor = 'salgado';
+      break;
+
+    case 'limão':
+        sabor = 'azedo';
+      break;
+
+    case 'café':
+        sabor = 'dmargo';
+      break;
+
+    case 'bolo':
+        sabor = 'doce';
+      break;
+
+    default:
+  }
+  print('O alimento é $alimento e o seu sabor é $sabor');
+}
