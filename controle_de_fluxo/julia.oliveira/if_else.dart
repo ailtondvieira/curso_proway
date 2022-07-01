2fffe69cea647fa5c3512f970f14b0c35c073d03
Júlia Luciani De Oliveira <Jlia.Oliveira@proway.treina>
2022-07-01 13:41:48 -0300

    [feat]

--- controle_de_fluxo/julia.oliveira/if_else.dart
+++ controle_de_fluxo/julia.oliveira/if_else.dart
@@ -7,4 +7,7 @@
     {
       print('Passe o cartão!!!');
     }
+
+  
+
   }
---
+++ controle_de_fluxo/julia.oliveira/while.dart
@@ -0,0 +1,23 @@
+void main() {
+  int contador = 1;
+  while (contador <= 50) {
+    print('Contando...$contador');
+    contador++;
+  }
+
+  int pessoasNaFila = 10;
+  int posicaoAtual = 10;
+
+  while (pessoasNaFila >= 0) {
+  
+    if (posicaoAtual == 0) {
+      print('Agora é sua vez');
+      break;
+    } else {
+      posicaoAtual--;
+      print('Sua posição na fila é $posicaoAtual');
+      pessoasNaFila--;
+
+    }
+  }
+}
