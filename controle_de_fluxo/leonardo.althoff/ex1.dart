15d79d0b26b0bcc2d72978c2958d6e01d190d975
Leonardo Menel Althoff <Leonardo.Althoff@proway.treina>
2022-07-01 11:55:28 -0300

    Exercício if-else.

---
+++ controle_de_fluxo/leonardo.althoff/ex1.dart
@@ -0,0 +1,9 @@
+void main() {
+  bool estaAtrasado = true;
+
+  if (estaAtrasado == true) {
+    print("Pule o café da manhã!");
+  } else {
+    print("Faça o seu café da manhã tranquilo.");
+  }
+}
