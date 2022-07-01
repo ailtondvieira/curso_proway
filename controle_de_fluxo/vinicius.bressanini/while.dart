f475d4e19a88a49aff675ecef8d44955ef873edf
Vinicius Marcus <vinimarcus41@gmail.com>
2022-07-01 13:40:24 -0300

    [feat} Uploading while exercises

---
+++ controle_de_fluxo/vinicius.bressanini/while.dart
@@ -0,0 +1,12 @@
+import 'dart:io';
+void main() {
+  String? word;
+
+  while (word != "Stop") {
+
+    print("Escreva qualquer coisa (Escreva Stop para parar)");
+    word = stdin.readLineSync();
+        
+  }
+
+}
