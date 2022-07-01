2d28e42ce15d8297b0983f8268cd98e8a20d1fe7
Carlos Aurélio Casanova de la Torre Ugarte <Carlos.Ugarte@proway.treina>
2022-07-01 15:10:22 -0300

    [change] assert file new asset

--- controle_de_fluxo/carlos.casanova/assert.dart
+++ controle_de_fluxo/carlos.casanova/assert.dart
@@ -1,10 +1,12 @@
 void main() {
-  Alunos aluno = Alunos('Ailton', 27);
+  Alunos aluno = Alunos('Carlos', 27);
 }
 
 class Alunos {
   String name;
   int idade;
 
-  Alunos(this.name, this.idade) : assert(name == 'Carlos', 'nome inválido');
+  Alunos(this.name, this.idade)
+      : assert(name == 'Carlos', 'nome inválido'),
+        assert(idade == 25, 'idade inválida');
 }
