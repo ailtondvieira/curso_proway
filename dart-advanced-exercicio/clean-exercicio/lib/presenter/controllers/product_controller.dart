0828f0fe1387fd1a566b14501b3d1b7e72615d5c
João Pedro De Arruda Rocha <Joo.Rocha@proway.treina>
2022-07-25 11:43:51 -0300

    [feat] Adding Exercise

--- dart-advanced-exercicio/clean-exercicio/lib/presenter/controllers/product_controller.dart
+++ dart-advanced-exercicio/clean-exercicio/lib/presenter/controllers/product_controller.dart
@@ -1,17 +1,17 @@
 
 import '../../domain/entities/product_entity.dart';
-import '../../domain/usecases/get_all_products_usecase_imp.dart';
+import '../../domain/usecases/get_all_products_usecase.dart';
 
 class ProductController {
-  final GetAllProductsUsecaseImp _getAllProductsUsecaseImp;
+  final IGetAllProductsUsecase _iGetAllProductsUsecase;
   ProductController(
-    this._getAllProductsUsecaseImp,
+    this._iGetAllProductsUsecase,
   ) {
     getAllProducts();
   }
 
   late List<ProductEntity> allProducts;
   void getAllProducts() {
-    allProducts = _getAllProductsUsecaseImp.call();
+    allProducts = _iGetAllProductsUsecase.call();
   }
 }
