81094fe50a0ba835a13fcc84492b7b38a30a028c
João Pedro De Arruda Rocha <Joo.Rocha@proway.treina>
2022-08-09 10:39:25 -0300

    g

--- flutter-advanced/local_database/stream_future_builder/lib/home_page.dart
+++ flutter-advanced/local_database/stream_future_builder/lib/home_page.dart
@@ -1,8 +1,10 @@
+import 'dart:async';
+
 import 'package:flutter/material.dart';
 import 'package:stream_future_builder/home_repository.dart';
 
 class HomePage extends StatefulWidget {
-  const HomePage({Key? key}) : super(key: key);
+  const HomePage([Key? key]) : super(key: key);
 
   @override
   State<HomePage> createState() => _HomePageState();
@@ -12,9 +14,16 @@ class _HomePageState extends State<HomePage> {
   final _repository = HomeRepository();
   late Future<List<String>> names;
   late Stream<int> counter;
+  final StreamController<List<String>> controller = StreamController();
+
+  void getAllNamesP() async {
+    List<String> names = await _repository.getAllNames();
+    controller.add(names);
+  }
 
   @override
   void initState() {
+    getAllNamesP();
     names = _repository.getAllNames();
     counter = _repository.timedCounter(const Duration(milliseconds: 250));
     super.initState();
@@ -38,14 +47,25 @@ class _HomePageState extends State<HomePage> {
               },
             ),
             StreamBuilder<int>(
-                stream: counter,
-                builder: (context, AsyncSnapshot<int> snapshot) {
-                  if (snapshot.hasData) {
-                    return Text(snapshot.data.toString());
-                  } else {
-                    return const CircularProgressIndicator();
-                  }
-                })
+              stream: counter,
+              builder: (context, AsyncSnapshot<int> snapshot) {
+                if (snapshot.hasData) {
+                  return Text(snapshot.data.toString());
+                } else {
+                  return const CircularProgressIndicator();
+                }
+              },
+            ),
+            StreamBuilder(
+              stream: controller.stream,
+              builder: (context, AsyncSnapshot<List<String>> snapshot) {
+                if (snapshot.hasData) {
+                  return Text(snapshot.data.toString());
+                } else {
+                  return const CircularProgressIndicator();
+                }
+              },
+            ),
           ],
         ),
       ),
