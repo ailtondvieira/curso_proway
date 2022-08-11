7954bfabba2c704284a7025266b80bd00d3c3d29
João Pedro <joao.pedro.dearruda.rocha@gmail.com>
2022-08-11 18:35:56 -0300

    j

--- flutter-advanced/revendo_api_riverpod_clean_arch/lib/src/presentation/riverpod/movies/movies_provider.dart
+++ flutter-advanced/revendo_api_riverpod_clean_arch/lib/src/presentation/riverpod/movies/movies_provider.dart
@@ -1,5 +1,7 @@
 import 'package:dio/dio.dart';
 import 'package:hooks_riverpod/hooks_riverpod.dart';
+import 'package:revendo_api_riverpod_clean_arch/src/data/datasources/remote/get_all_trending_movies_remote_datasource_imp.dart';
+import 'package:revendo_api_riverpod_clean_arch/src/data/repositories/get_all_trending_movies_repository_imp.dart';
 import 'package:revendo_api_riverpod_clean_arch/src/domain/usecases/get_all_trending_movies/get_all_trending_movies_usecase_imp.dart';
 import 'package:revendo_api_riverpod_clean_arch/src/presentation/riverpod/movies/movies_notifier.dart';
 
@@ -7,10 +9,26 @@ final dioProvider = Provider(
   (ref) => Dio(),
 );
 
+final moviesDatasourceProvider = Provider(
+  (ref) => GetAllTrendingMoviesRemoteDatasourceImp(
+    ref.watch(dioProvider),
+  ),
+);
+
+final moviesRepositoryProvider = Provider(
+  (ref) => GetAllTrendingMoviesRepositoryImp(
+    ref.watch(moviesDatasourceProvider),
+  ),
+);
+
 final moviesUseCaseProvider = Provider(
-  (ref) => GetAllTrendingMoviesUsecaseImp(_repository),
+  (ref) => GetAllTrendingMoviesUsecaseImp(
+    ref.watch(moviesRepositoryProvider),
+  ),
 );
 
 final moviesNotifierProvider = StateNotifierProvider(
-  (ref) => MoviesNotifier(ref.watch(moviesUseCaseProvider)),
+  (ref) => MoviesNotifier(
+    ref.watch(moviesUseCaseProvider),
+  ),
 );
